478d95aee87b99d9026463acb127ed47182dfe6f
Mahmoud Qandeel <99659529+Qandeeil@users.noreply.github.com>
2022-10-14 16:08:59 +0300

    Update Connections.js

--- API/Connections.js
+++ API/Connections.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Qandeeil:SURKnjiWcWPQzV3j@cluster0.wqfwxbi.mongodb.net/chatApp', {useNewUrlParser: true});
+mongoose.connect('********************', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
